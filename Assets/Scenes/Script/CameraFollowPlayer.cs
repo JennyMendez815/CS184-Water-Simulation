1760089ca490969b4469b7225da926c327b41f12
David Ge <lwg0320@berkeley.edu>
2023-05-02 12:57:19 -0700

    Fixed Camera and no more movement

--- Assets/Scenes/Script/CameraFollowPlayer.cs
+++ Assets/Scenes/Script/CameraFollowPlayer.cs
@@ -4,11 +4,23 @@ using UnityEngine;
 
 public class CameraFollowPlayer : MonoBehaviour
 {
-    public Transform player;
-    public float pLerp = 0.02f;
-    public float rLerp = 0.02f;
+    public Transform orientation;
+
+    private float xRotation = 0;
+    private float yRotation = 0;
+
+
     void Update() {
-        transform.position = Vector3.Lerp(transform.position, player.position, pLerp);
-        transform.rotation = Quaternion.Lerp(transform.rotation, player.rotation, rLerp);
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+        float x = Input.GetAxisRaw("Mouse X") * Time.deltaTime * 400;
+        float y = Input.GetAxisRaw("Mouse Y") * Time.deltaTime * 400;
+
+        xRotation += x;
+        yRotation += y;
+        //xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        transform.rotation = Quaternion.Euler(-yRotation, xRotation, 0);
+        orientation.rotation = Quaternion.Euler(0, yRotation, 0);
     }
 }
